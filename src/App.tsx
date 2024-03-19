570420e0654cf8df7b1ae30cf5112f977fdd7482
wycliffe <ericoochieng456@gmail.com>
2024-03-20 00:18:16 +0300

    Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import HomeUpdate from './assets/newComponent'
-import Counters from './assets/counters'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import TodoApp from './assets/TodoApp'
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
